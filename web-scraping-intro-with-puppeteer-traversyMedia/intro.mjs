d5f91325b62109e9c7f6361f47e63cba77076b26
Md. Mahadi Hassan Riyadh <mahadihassanriyadh1@gmail.com>
2023-09-15 23:57:03 +0600

    done with assignment
    Signed-off-by: Md. Mahadi Hassan Riyadh <mahadihassanriyadh1@gmail.com>

--- web-scraping-intro-with-puppeteer-traversyMedia/intro.mjs
+++ web-scraping-intro-with-puppeteer-traversyMedia/intro.mjs
@@ -1,20 +1,31 @@
 import puppeteer from "puppeteer";
+import { setTimeout } from "timers/promises";
 
 const browser = await puppeteer.launch({
     headless: false,
-    defaultViewport: {width: 1920, height: 1080},
-    slowMo: 250,
+    defaultViewport: { width: 1920, height: 1080 },
+    // slowMo: 250,
     userDataDir: "temp",
 });
 
 const page = await browser.newPage();
-await page.goto("https://www.google.com/", {
-    waitUntil: "networkidle2",
-    timeout: 60000,
-});
-await page.screenshot({ path: "google-screenshot.png" });
+// await page.goto("https://www.google.com/", {
+//     waitUntil: "networkidle2",
+//     timeout: 60000,
+// });
+// await page.screenshot({ path: "google-screenshot.png" });
 
 await page.goto("https://devconfbd.com/");
 await page.screenshot({ path: "devconfbd-screenshot.png" });
+// await page.waitForSelector("img[alt='guest']");
+// await page.click("img[alt='guest']");
+// await setTimeout(() => { }, 1000);
+// await page.screenshot({ path: "guest.png" });
+const guestElement = await page.waitForSelector("img[alt='guest']");
+await guestElement.scrollIntoView();
+await setTimeout(1000);
+await guestElement.click();
+await setTimeout(1000);
+await page.screenshot({ path: "guest.png" });
 
 await browser.close();
